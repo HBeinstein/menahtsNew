ba98bd119dd90cffbda6cd985f81bcb5e7a4e740
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-02 20:53:27 -0700

    add p5 js play/pause button for songone

--- songone.js
+++ songone.js
@@ -1,7 +1,23 @@
-const target=document.body;
+let song;
+let button;
 
-console.log(target);
+function setup() {
+//   createCanvas(100, 100);
+  song = loadSound('./audio/song1.wav');
+  button = createButton("Play");
+  button.position(19, 19);
+  button.mousePressed(togglePlaying)
+}
 
-target.addEventListener("click", function(){
-    console.log("sgfdhfg")
-})
+function togglePlaying() {
+    if (song.isPlaying()) {
+        // .isPlaying() returns a boolean
+        song.stop();
+        button.html("Play")
+        // background(255, 0, 0);
+      } else {
+        song.play();
+        button.html("Pause")
+        // background(0, 255, 0);
+      }
+}
